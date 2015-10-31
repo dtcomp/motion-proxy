a182684e36ec8f6adc73e63115dd09f3fffd903e
dtcomp <dtc@mtaonline.net>
2015-10-31 02:38:01 -0800

    More tidying...

--- MotionProxy/Config.pm
+++ MotionProxy/Config.pm
@@ -7,75 +7,81 @@ use warnings;
 
 use Log::Lite qw(logrotate logmode logpath log);
 use Config::General;
+
 # From http://search.cpan.org/~tlinden/Config-General-2.60/General/Extended.pm
 use Data::Dumper;
 
 sub new {
-  my $class=shift;
-  my $file=shift;
-  my $defaults=shift;
-  my %rootvalues;
-  my $self = bless {
-    file  => $file,
-    conf  => undef,
-    data  => undef,
-    value => \%rootvalues
-  }, $class;
-  $self->setDefaults($defaults);
-  $self->initialize();
+    my $class    = shift;
+    my $file     = shift;
+    my $defaults = shift;
+    my %rootvalues;
+    my $self = bless {
+        file  => $file,
+        conf  => undef,
+        data  => undef,
+        value => \%rootvalues
+    }, $class;
+    $self->setDefaults($defaults);
+    $self->initialize();
 
-  return $self;
+    return $self;
 }
 
 sub setDefaults {
-  my $self=shift;
-  my $href=shift;
-  
-#  print "href: ", Dumper($href), "\n";
-  for my $k (keys %{$href}) {
-#    print "Setting default for $k => $href->{$k} \n";
-    $self->{value}->{$k} = $href->{$k};
-  }
+    my $self = shift;
+    my $href = shift;
+
+    #  print "href: ", Dumper($href), "\n";
+    for my $k ( keys %{$href} ) {
+
+        #    print "Setting default for $k => $href->{$k} \n";
+        $self->{value}->{$k} = $href->{$k};
+    }
 }
 
 sub get {
-  my $self=shift;
-  my $key=shift;
-  
-  if (exists $self->{value}->{$key}) {
-#    print "key $key => $self->{value}->{$key} \n";
-    return $self->{value}->{$key};
-  } else {
-    die("$key does not exist!");
-  }
+    my $self = shift;
+    my $key  = shift;
+
+    if ( exists $self->{value}->{$key} ) {
+
+        # print "key $key => $self->{value}->{$key} \n";
+        return $self->{value}->{$key};
+    }
+    else {
+        die("$key does not exist!");
+    }
 }
 
 sub initialize {
-    my $self=shift;
-    
+    my $self = shift;
 
-    
     $self->{conf} = Config::General->new(
-        -ConfigFile     => $self->{file},
-        -ExtendedAccess => 1,
+        -ConfigFile       => $self->{file},
+        -ExtendedAccess   => 1,
         -ApacheCompatible => 1
     );
- 
+
     my %data = $self->{conf}->getall();
-#    print "data: ", Dumper(%data), "\n";
+
+    #    print "data: ", Dumper(%data), "\n";
 
     my @keys = keys %data;
     for my $key (@keys) {
- #       print "key = $key, val =" . $data{$key} . "\n";
-        if ( $key  eq 'Camera' ) {
-                my @cameras = @{$data{$key}};
-                my $len = scalar @cameras;
-                foreach my $cam (@cameras) {
-                  my $newcam = new MotionProxy::Camera($cam);
-#                  print "newcam: ", Dumper($newcam), "\n";
-                }
-        } else {      # Assume top-level value
-              $self->{value}->{$key} = $data{$key};
+
+        #       print "key = $key, val =" . $data{$key} . "\n";
+        if ( $key eq 'Camera' ) {
+            my @cameras = @{ $data{$key} };
+            my $len     = scalar @cameras;
+            foreach my $cam (@cameras) {
+                my $newcam = new MotionProxy::Camera($cam);
+
+                #                  print "newcam: ", Dumper($newcam), "\n";
+            }
+        }
+        else {    # Assume top-level value
+            $self->{value}->{$key} = $data{$key};
         }
     }
     $self->{data} = \%data;
--- MotionProxy/Queue.pm
+++ MotionProxy/Queue.pm
@@ -1,11 +1,14 @@
 package MotionProxy::Queue;
- use threads ('yield',
-'stack_size' => 128*4096,
-'exit' => 'threads_only',
-'stringify');
+use threads (
+    'yield',
+    'stack_size' => 128 * 4096,
+    'exit'       => 'threads_only',
+    'stringify'
+);
 use strict;
 use diagnostics;
 use warnings;
+
 #use Data::Dumper;
 use Log::Lite qw(logrotate logmode logpath log);
 use HTTP::Daemon;
@@ -17,193 +20,200 @@ use MotionProxy::QueueObject;
 use MotionProxy::Constant;
 
 sub new {
-  my $class=shift;
-  my $camera=shift;  # MotionProxyCamera
-  my $data=[];
-  my $skipped=0;
-  my $total=0;
-  my $last=undef;
-  my $daemon=undef;
-  my $thread=undef;
+    my $class   = shift;
+    my $camera  = shift;    # MotionProxyCamera
+    my $data    = [];
+    my $skipped = 0;
+    my $total   = 0;
+    my $last    = undef;
+    my $daemon  = undef;
+    my $thread  = undef;
 
+    my $self = bless {
+        camera  => $camera,
+        data    => $data,
+        skipped => $skipped,
+        last    => $last,
+        total   => $total,
+        daemon  => $daemon,
+        thread  => $thread
+    }, $class;
 
-  my $self = bless {
-    camera  => $camera,
-	data    => $data,
-	skipped => $skipped,
-	last    => $last,
-	total   => $total,
-	daemon  => $daemon,
-	thread  => $thread
-  }, $class;
-  
-  $self->initialize();
-  return $self;
+    $self->initialize();
+    return $self;
 }
 
 sub initialize {
-  my $self=shift;
-  
-  $self->fill();
-  $self->{thread} =  async { $self->run(); };
-  print "GOT HERTE\n";
-  $self->{thread}->yield();
-#  $self->run();
+    my $self = shift;
+
+    $self->fill();
+    $self->{thread} = async { $self->run(); };
+    print "GOT HERTE\n";
+    $self->{thread}->yield();
+
+    #  $self->run();
 }
 
 sub p {
-  my $self=shift;
-  
-  print "camera  ", $self->{camera}, "\n";
-  my $ar=$self->{data};
-  print "data    ", @$ar[0],  "\n";
-  print "last    ", $self->{last}, "\n";
-  print "total   ", $self->{total}, "\n";
-  print "skipped ", $self->{skipped}, "\n";
-  print "daemon  ", $self->{daemon}, "\n";
+    my $self = shift;
+
+    print "camera  ", $self->{camera}, "\n";
+    my $ar = $self->{data};
+    print "data    ", @$ar[0], "\n";
+    print "last    ", $self->{last},    "\n";
+    print "total   ", $self->{total},   "\n";
+    print "skipped ", $self->{skipped}, "\n";
+    print "daemon  ", $self->{daemon},  "\n";
 }
 
 sub sort {
-  my $self=shift;
-  my $ar = $self->{data};
-  @{$ar} = sort { return  $a->{time} <=> $b->{time}; } $ar;
+    my $self = shift;
+    my $ar   = $self->{data};
+    # Sort by ctime
+    @{$ar} = sort { return $a->{time} <=> $b->{time}; } $ar;
 }
 
 sub fill {
-  my $self=shift;
-  my $dir = $self->{camera}->{inpath};
-  my $dh = DirHandle->new($dir);
-  my $minsize=$self->{camera}->{minsize};
-  my $maxsize=$self->{camera}->{maxsize};
-  my @stat;
-  
-  while ( defined( $_ = $dh->read() )) {
-    next unless (/\.jpg$/i);
- #   print "$dir : file= $_ \n";
-    my $full_path = File::Spec->catfile($dir, $_);
-    @stat = (stat $full_path);
-    my $bytes = $stat[7];
-    if ( $bytes < $minsize ) {
-      log(MotionProxy::Constant::LOGNAME,"Bytes: $bytes < $minsize, $full_path. ");
-      unlink $full_path;
-      ++$self->{skipped};
-      $self->{camera}->minsizeEvent();
-      next;
+    my $self    = shift;
+    my $dir     = $self->{camera}->{inpath};
+    my $dh      = DirHandle->new($dir);
+    my $minsize = $self->{camera}->{minsize};
+    my $maxsize = $self->{camera}->{maxsize};
+    my @stat;
+
+    while ( defined( $_ = $dh->read() ) ) {
+        next unless (/\.jpg$/i);
+
+        #   print "$dir : file= $_ \n";
+        my $full_path = File::Spec->catfile( $dir, $_ );
+        @stat = ( stat $full_path );
+        my $bytes = $stat[7];
+        if ( $bytes < $minsize ) {
+            log( MotionProxy::Constant::LOGNAME,
+                "Bytes: $bytes < $minsize, $full_path. " );
+            unlink $full_path;
+            ++$self->{skipped};
+            $self->{camera}->minsizeEvent();
+            next;
+        }
+        if ( $bytes > $maxsize ) {
+            log( MotionProxy::Constant::LOGNAME,
+                "Bytes: $bytes > $maxsize, $full_path. " );
+            unlink $full_path;
+            ++$self->{skipped};
+            $self->{camera}->maxsizeEvent();
+            next;
+        }
+        my $time = $stat[10];
+        $self->enqueue(
+            new MotionProxy::QueueObject( $self, $full_path, $time, $bytes ) );
     }
-    if ( $bytes > $maxsize ) {
-      log(MotionProxy::Constant::LOGNAME,"Bytes: $bytes > $maxsize, $full_path. ");
-      unlink $full_path;
-      ++$self->{skipped};
-      $self->{camera}->maxsizeEvent();
-      next;
-    }
-    my $time = $stat[10];
-    $self->enqueue( new MotionProxy::QueueObject( $self, $full_path, $time, $bytes ) );
-  }
-  $self->sort();
+    $self->sort();
 }
 
 sub enqueue {
-  my $self=shift;
-  my $obj=shift;
-  my $ar=$self->{data};
-  push $ar, $obj;
+    my $self = shift;
+    my $obj  = shift;
+    my $ar   = $self->{data};
+    push $ar, $obj;
 }
 
 sub dequeue {
-  my $self=shift;
-  my $ar=$self->{data};
-  return pop $ar;
+    my $self = shift;
+    my $ar   = $self->{data};
+    return pop $ar;
 }
 
 sub next {
-  my $self=shift;
-  my $ar=$self->{data};
-  my $n=scalar @$ar;
+    my $self = shift;
+    my $ar   = $self->{data};
+    my $n    = scalar @$ar;
 
-  if ($n < 1 ) {
-    $self->{last}=undef;
-    $self->fill();
-  }
-  my $obj = shift $ar;
-  $self->{last} = $obj;
-  return $self->{last};
+    if ( $n < 1 ) {
+        $self->{last} = undef;
+        $self->fill();
+    }
+    my $obj = shift $ar;
+    $self->{last} = $obj;
+    return $self->{last};
 }
-
 
 use POSIX qw(:sys_wait_h);
 
 sub run {
-  my $self=shift;
-  my $daemon;
-  my $sport=$self->{camera}->{port};
-  
+    my $self = shift;
+    my $daemon;
+    my $sport = $self->{camera}->{port};
+
     $daemon = HTTP::Daemon->new(
-#	  LocalAddr => 'localhost',
-	  LocalPort => $self->{camera}->{port},
-	  ReuseAddr => 1
+
+        #	  LocalAddr => 'localhost',
+        LocalPort => $self->{camera}->{port},
+        ReuseAddr => 1
     ) || die;
 
-  print "Please contact ", $self->{camera}->{name},  "  at ", $daemon->url, "\n";
-  my $path = $self->{camera}->{path};
-  my $conn=undef;
+    print "Please contact ", $self->{camera}->{name}, "  at ", $daemon->url,
+      "\n";
+    my $path = $self->{camera}->{path};
+    my $conn = undef;
 
-  print "Wait for accept \n";
-  yield();
- 
-  while  ( $conn = $daemon->accept() ) {
-#	print "Connect from host: ", $conn->peerhost(), "$sport .\n";
-    while (my $request = $conn->get_request) {
-#	  my $rhost = $request->uri->host;
-#	  my $rport = $request->uri->port;
-	  my $rpath = $request->uri->path;
-      if ($request->method eq 'GET' and $rpath eq $path ) {
-		my $obj = $self->next();
-		if (defined($obj)) {
-	      my $file = $obj->{name};
-	      if ($file) {
-#		    print "Sending: ", $file, "  \n";
-            $conn->send_file_response( $file );
-			$obj->done();
-#		    yield();
-          } else {
-		    print("No filename to send!???.\n");
-			$conn->send_error(500);
-#			yield();
-	      }
-		} else {  # out of files to send...
-		  print "out of files...\n";
-#		  $conn->send_error(RC_NOT_FOUND);
-#		  yield();
-		}
-		print "got here 1\n";
-	  } else {
-	    print "Got here 2: 403 ($rpath) \n";
-        $conn->send_error(RC_FORBIDDEN);
-#		 $conn->send_response(RC_NOT_MODIFIED);
-#		yield();
-      }
-	  print "got here 3 \n";
+    print "Wait for accept \n";
+    yield();
+
+    while ( $conn = $daemon->accept() ) {
+
+        # print "Connect from host: ", $conn->peerhost(), "$sport .\n";
+        while ( my $request = $conn->get_request ) {
+            #	  my $rhost = $request->uri->host;
+            #	  my $rport = $request->uri->port;
+            my $rpath = $request->uri->path;
+            if ( $request->method eq 'GET' and $rpath eq $path ) {
+                my $obj = $self->next();
+                if ( defined($obj) ) {
+                    my $file = $obj->{name};
+                    if ($file) {
+                        # print "Sending: ", $file, "  \n";
+                        $conn->send_file_response($file);
+                        $obj->done();
+                    }
+                    else {
+                        print("No file to send!?  ($file)\n");
+                        $conn->send_error(500);
+                    }
+                }
+                else {    # out of files to send...
+                    print "out of files...\n";
+                    #		  $conn->send_error(RC_NOT_FOUND);
+                }
+#                print "got here 1\n";
+            }
+            else {
+#                print "Got here 2: 403 ($rpath) \n";
+                $conn->send_error(RC_FORBIDDEN);
+                # $conn->send_response(RC_NOT_MODIFIED);
+                # yield();
+            }
+#            print "got here 3 \n";
+        }
+#        print "Got here 4 \n";
+
+        #    if ($conn) {
+        #	   print "Closing conn.\n";
+        #	   $conn->force_last_request;
+        #       $conn->close();
+        #       undef($conn);
+        #     }
     }
-    print "Got here 4 \n";
-#    if ($conn) {
-#	   print "Closing conn.\n";
-#	   $conn->force_last_request;
-#       $conn->close();
-#       undef($conn);
-#     }
-  } 
-  print "UHOH, something bad happened...\n";
+    print __FILE__:__LINE__, "UHOH, something bad happened...\n";
 }
 
 sub stop {
-  my $self=shift;
-  die(" stop Not Implemented");
+    my $self = shift;
+    die("Stop Not Implemented");
 }
 
 # print __FILE__ . ": @INC: \n";
 
 1;
-
 
 __END__
